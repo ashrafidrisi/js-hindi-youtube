c14317eed3144af22fcd51f3155aed76a4b31b0b
ashrafidrisi <140957619+ashrafidrisi@users.noreply.github.com>
2023-12-13 13:28:03 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // Treat all js code as newer version
+ // alert(3+3)  // we are using nodejs,not browser
+
+  console.log(3+3);
+  console.log("ashraf");
+
+  let name="mohammad"
+  let age=26
+  let inloggein=false
+
+  // number
+  // bigint
+  // string => ""
+  // bolean => true/false
+  // null => standalone value // yeah ek khali value hai
+    // let state;
+  // undefine // state ki value undefine hai
+  // symbol=> unique
+    
+  console.log(typeof "ashraf"); // string
+  console.log(typeof age);  // number
+
+  console.log(typeof undefined) // undefined
+  console.log(typeof null) // object
