c8fff9b614dcf5d8ee878ca571c546b90025dc09
ashrafidrisi <140957619+ashrafidrisi@users.noreply.github.com>
2023-12-13 15:01:21 +0000

    conversion of dataType

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,94 @@
+// let score=33
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let score="33"
+// console.log(typeof score);
+// console.log(typeof (score));
+// string ki value ko number me convert karne ka tarikah
+//  let valueInNumber= Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// string
+// string
+// number
+// 33
+
+// let score="33abc"
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber= Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// string
+// string
+// number
+// NaN
+
+// let score=null
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber= Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// object
+// object
+// number
+// 0
+
+// let score=undefined
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber= Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// undefined
+// undefined
+// number
+// NaN
+
+// let score=true
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber= Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// boolean
+// boolean
+// number
+// 1
+
+// let score="ashraf"
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber= Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// string
+// string
+// number
+// NaN
+
+// let inloggein=1
+// let booleaninloggein=Boolean(inloggein)
+// console.log(booleaninloggein);
+
+// 1 => true,  0=> false
+// "" => false
+// "ashraf" => true
+
+ let someNumber=33
+ let stringNumber=String(someNumber)
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
