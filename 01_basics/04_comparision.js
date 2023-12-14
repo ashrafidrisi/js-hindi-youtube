ffb58ec165dd5fa002c03fd2b573e3ea11a41eb6
ashrafidrisi <140957619+ashrafidrisi@users.noreply.github.com>
2023-12-14 06:07:42 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,22 @@
+// console.log("2" > 1)
+// console.log("02" > 1)
+
+// true
+// true
+
+// console.log(null > 0)
+// console.log( null == 0)
+// console.log(null >= 0);
+// console.log(null <= 0);
+
+// false
+// false
+// true
+// true
+
+console.log(undefined > 0)
+console.log( undefined == 0)
+console.log(undefined >= 0);
+console.log(undefined <= 0);
+
+// false all answer
