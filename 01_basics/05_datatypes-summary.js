8348c350b8674a05f6679de5611cacd3dd4e768f
ashrafidrisi <140957619+ashrafidrisi@users.noreply.github.com>
2023-12-14 06:54:31 +0000

    datatypes-summary

---
+++ 01_basics/05_datatypes-summary.js
@@ -0,0 +1,23 @@
+// primitive
+
+// 7 types  ,,  string,number,boolean,null,undefined,symbol,bigInt
+
+// non-primitive(reference) ,,  array,object, functions
+
+//   const hero = ["ashraf","nahid","saleem"];
+
+//    const myobj = {
+//     name:"mohammad",
+//      age:25
+//    }
+
+// array,object ka data type ,,, object hota hai
+
+    // const mufunction = function(){
+    //     console.log("hello");
+    // }
+
+    // data type function hota hai bolte hai function object
+
+    const bigInt= 127823367792694402n;
+    console.log(typeof bigInt);
