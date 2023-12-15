01a2f6e721d0e1d277cdf2c01b7186ec2dfc2e55
ashrafidrisi <140957619+ashrafidrisi@users.noreply.github.com>
2023-12-15 05:30:13 +0000

    addoutput

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -16,3 +16,22 @@
    accountPassword="azmi56";
    accountCity="azamgarh";
    console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+//   ┌─────────┬────────────────────┐
+//   │ (index) │       Values       │
+//   ├─────────┼────────────────────┤
+//   │    0    │       13345        │
+//   │    1    │ 'ashraf@gmail.com' │
+//   │    2    │     'ashraf97'     │
+//   │    3    │      'jaipur'      │
+//   └─────────┴────────────────────┘
+//   13345
+//   ┌─────────┬──────────────────┐
+//   │ (index) │      Values      │
+//   ├─────────┼──────────────────┤
+//   │    0    │      13345       │
+//   │    1    │ 'azmi@gmail.com' │
+//   │    2    │     'azmi56'     │
+//   │    3    │    'azamgarh'    │
+//   │    4    │    undefined     │
+//   └─────────┴──────────────────┘
