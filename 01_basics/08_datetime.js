3bf8a8b41cf82d4ee668baee8d0adceddaa54b59
ashrafidrisi <140957619+ashrafidrisi@users.noreply.github.com>
2023-12-15 14:05:26 +0000

    date

---
+++ 01_basics/08_datetime.js
@@ -0,0 +1,27 @@
+ // dates
+//   let Mydate = new Date()
+//   console.log(typeof Mydate);  // object
+//   console.log(Mydate);   //  2023-12-15T13:16:21.587Z
+//   console.log(Mydate.toString());  //  Fri Dec 15 2023 13:16:21 GMT+0000 (Coordinated Universal Time)
+//   console.log(Mydate.toLocaleString());  //   12/15/2023, 1:16:21 PM
+
+// let createMydate = new Date(2023,0,23)  //  Mon Jan 23 2023
+
+// let createMydate = new Date("12-15-2023")  /// Fri Dec 15 2023
+//  console.log(createMydate.toDateString());
+
+//    let createMydates = new Date("12-15-2023")
+
+//   let myTimes = Date.now()
+//   console.log(myTimes);
+//   console.log(createMydates.getTime());  //  1702647771297   mili second me ans hai
+
+//  console.log(Math.floor(Date.now()/1000));   // 1702647803  yeah ans second me hai
+
+   let newdate = new Date()
+    // console.log(newdate);  ///   2023-12-15T13:58:54.220Z
+
+    // console.log(newdate.getMonth());  //   11
+
+    // console.log(newdate.getDay());  //    5
+
