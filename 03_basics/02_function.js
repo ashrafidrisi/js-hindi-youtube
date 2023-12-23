800e5cb478a96c1d91b4533ef8ccab667c239aae
ashrafidrisi <140957619+ashrafidrisi@users.noreply.github.com>
2023-12-23 12:27:20 +0000

    function2

--- 03_basics/02_function.js
+++ 03_basics/02_function.js
@@ -1,1 +1,20 @@
-console.log("hello")
+//  if(true){
+//     const a=2
+//     let b=5
+//     var c=10
+
+//  }
+ // console.log(a);  // error
+  // console.log(b);  // error
+ // console.log(c);  //  10
+
+//  const a=2
+//     let b=5
+//  if(true){
+    
+//    console.log(a); 
+//    console.log(b);
+
+//  }
+//  2
+//  5
