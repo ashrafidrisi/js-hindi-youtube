56eb433f604b204424043037d10e70013ca37879
ashrafidrisi <140957619+ashrafidrisi@users.noreply.github.com>
2023-12-24 19:25:04 +0000

    one

---
+++ 05_iteration/one.js
@@ -0,0 +1,68 @@
+  // for 
+   
+    // for(let i=0; i<=10;i++){
+    //     const element = i;
+    //     console.log(element);
+    // }
+
+    // for(let i=0; i<=10;i++){
+    //     const element = i;
+     
+    //     if(i==5){
+    //         console.log("5 is best number");
+    //     }
+          
+    //     console.log(element);
+    // }
+
+      // for(let i = 1; i<=4; i++){
+      //   console.log(`outer loop ${i}`)
+      //   for(let j=1; j<=4; j++){
+      //     console.log(`${i} * ${j} = ${i*j}`);
+      //   }
+      // }
+
+      //  let myArray = ["mohammad","ashraf","azmi"]
+      //    console.log(myArray.length);
+      //    for(let i=0; i<myArray.length; i++ ){
+      //      let element = myArray[i]
+      //      console.log(element);
+      //    }
+
+      //  for (let i=1; i<=20; i++) {
+
+      //     if(i==5){
+      //       console.log(`Detected 5`);
+      //       break
+      //     }
+      //   console.log(`value of i is ${i}`);
+        
+      //  }
+           
+      //  value of i is 1
+      //  value of i is 2
+      //  value of i is 3
+      //  value of i is 4
+      //  Detected 5
+
+
+      for (let i=1; i<=10; i++) {
+
+        if(i==5){
+          console.log(`Detected 5`);
+          continue
+        }
+      console.log(`value of i is ${i}`);
+      
+     }
+
+    //  value of i is 1
+    //  value of i is 2
+    //  value of i is 3
+    //  value of i is 4
+    //  Detected 5
+    //  value of i is 6
+    //  value of i is 7
+    //  value of i is 8
+    //  value of i is 9
+    //  value of i is 10
