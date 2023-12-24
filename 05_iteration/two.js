28bc2ab69afc081ae8a399e4830075eb147a5b57
ashrafidrisi <140957619+ashrafidrisi@users.noreply.github.com>
2023-12-24 19:44:52 +0000

    two

---
+++ 05_iteration/two.js
@@ -0,0 +1,32 @@
+  
+//    let i=0
+//  while (i<=10) {
+//     console.log(`Value is ${i}`);
+//     i = i+2
+//  }
+  
+// Value is 0
+// Value is 2
+// Value is 4
+// Value is 6
+// Value is 8
+// Value is 10
+
+//    let myArray = ["mohammad","ashraf","azmi"]
+//      let i=0
+
+//      while (i<myArray.length) {
+//         console.log(` Value is ${myArray[i]}`);
+//           i = i+1
+//      }  
+
+//      Value is mohammad
+//      Value is ashraf
+//      Value is azmi  
+
+
+    let i=1
+       do {
+          console.log(`Value is ${i}`);
+          i++
+       } while (i<=10);
