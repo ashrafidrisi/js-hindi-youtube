493f050836d5f858fb61a0b629aee9f4678b58fd
ashrafidrisi <140957619+ashrafidrisi@users.noreply.github.com>
2023-12-24 13:52:35 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,40 @@
+
+//    const month =3
+
+//     switch (month) {
+//         case 1:
+//             console.log('jan');
+//             break;
+//         case 2:
+//             console.log('feb');
+//             break;
+//         case 3:
+//             console.log('march');
+//             break;
+    
+//         default:
+//             console.log('default value');
+//             break;
+//     }
+
+
+const month ="march"
+
+switch (month) {
+    case "jan":
+        console.log('jan');
+        break;
+    case "feb":
+        console.log('feb');
+        break;
+    case "march":
+        console.log('march');
+        break;
+    case "april":
+        console.log('april');
+        break;
+
+    default:
+        console.log('default value');
+        break;
+}
