2fad66c2e56d292d8e17a2945ad082e998d92af3
ashrafidrisi <140957619+ashrafidrisi@users.noreply.github.com>
2023-12-25 11:15:29 +0000

    three

---
+++ 05_iteration/three.js
@@ -0,0 +1,49 @@
+ //  for of  
+    // let arr = [1,2,3,4,5]
+    // for (const num of arr) {
+    //     console.log(num);
+    // }
+
+      // const greeting = "hello world"
+      //   for(let i of greeting){
+      //       console.log(`Each Char is ${i}`);
+      //   }
+
+
+    // let arr = [1,2,3,4,5]
+    // for (const num in arr) {
+    //     console.log(num);
+    // }
+      
+        let mapp = new Map()
+          mapp.set('IN', 'India')
+          mapp.set('USA', 'United State of America')
+         // console.log(mapp);
+
+         // Map(2) { 'IN' => 'India',
+         //         'USA' => 'United State of America'
+         //        }
+           
+      //    for (const key of mapp) {
+      //     console.log(key);
+      //  }
+
+      //  [ 'IN', 'India' ]
+      //  [ 'USA', 'United State of America' ]
+
+          // for (const [key, value] of mapp) {
+          //    console.log(key, value);
+          // }
+
+          // IN India
+          // USA United State of America
+
+          // const obj = {
+          //   name : "ashraf",
+          //   age :25
+          // }
+          // for (const [key,value] of obj) {
+          //   console.log(key, value);
+          // }
+
+          // TypeError: obj is not iterable
