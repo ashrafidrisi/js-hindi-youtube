eaf66aa22e9ea99ee17208601b1406a69f9679b9
ashrafidrisi <140957619+ashrafidrisi@users.noreply.github.com>
2023-12-25 19:21:19 +0000

    six

---
+++ 05_iteration/six.js
@@ -0,0 +1,31 @@
+ // forEach function value return nahi kerta hai
+//  const coding = ["java","python","js","html"] 
+
+//         const retuvalue = coding.forEach( (item)=>{
+        // console.log(item);
+        //  } )
+        //  console.log(retuvalue);
+
+        //   undefined
+
+        //  const myArray = [2,4,6,7,9]
+           //myArray.filter( (num)=> num>4 )
+          // myArray.filter( (num)=> (num>4) )
+        //    const retval = myArray.filter( (num)=> {
+        //        return num>4
+        //     } )
+        //     console.log(retval);
+
+        //     [ 6, 7, 9 ]
+
+
+        // const myArray = [2,4,6,7,9]       
+        //     const myNum = []
+        //      myArray.forEach( (num)=>{
+        //         if(num>4){
+        //             myNum.push(num)
+        //         }
+        //      } )
+        //      console.log(myNum);
+
+        //      [ 6, 7, 9 ]
