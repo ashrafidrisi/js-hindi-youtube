11d236edeedb83d60ac8da8ec3cde5c77e221b2e
ashrafidrisi <140957619+ashrafidrisi@users.noreply.github.com>
2023-12-26 07:17:45 +0000

    seven

---
+++ 05_iteration/seven.js
@@ -0,0 +1,35 @@
+
+ const myNumbers = [1,2,3,4,5,6,7,8,9,10]
+//     const Myn = myNumbers.map( (num)=> { return num + 10})
+//    console.log(Myn);
+
+//    [
+//     11, 12, 13, 14, 15,
+//     16, 17, 18, 19, 20
+//   ]
+
+// const Myn = myNumbers
+//             .map( (num)=> num * 10)
+           // .map((num)=> num +1)           // yeah per num ki value 10,20 .....   ho jayege phir 1 add hoga
+    // console.log(Myn);
+
+    //  [
+    //     11, 21, 31, 41,  51,
+    //     61, 71, 81, 91, 101
+    //   ]
+
+
+    // const Myn = myNumbers
+    //             .map( (num)=> num * 10)
+    //             .map((num)=> num +1) 
+    //             .filter( (num)=> num >=40 )
+    // console.log(Myn);    
+    
+    // [
+    //     41, 51,  61, 71,
+    //     81, 91, 101
+    //   ]
+
+    console.log(Myn);
+
+        
