33d892971d0b1d07537271bca2d0f07c972328a7
ashrafidrisi <140957619+ashrafidrisi@users.noreply.github.com>
2023-12-26 07:17:28 +0000

    modify

--- 05_iteration/six.js
+++ 05_iteration/six.js
@@ -29,3 +29,62 @@
         //      console.log(myNum);
 
         //      [ 6, 7, 9 ]
+
+         const Books = [
+          {title :'Book One' , genre : 'Friction', publish : 1981 , edition : 2004},
+          {title :'Book Two' , genre : 'Non-Friction', publish : 1961 , edition : 1994},
+          {title :'Book Three' , genre : 'History', publish : 1967 , edition : 1987},
+          {title :'Book Four' , genre : 'Math', publish : 1955 , edition : 1978},
+          {title :'Book Five' , genre : 'Physics', publish : 1881 , edition : 1904},
+          {title :'Book Six' , genre : 'History', publish : 1907 , edition : 1950},
+          {title :'Book Seven' , genre : 'Math', publish : 1945 , edition : 1969},
+          
+         ]
+
+          //  const UserBooks = Books.filter( (bk)=> bk.genre === 'History')
+          //   console.log(UserBooks);
+
+            // [
+            //   {
+            //     title: 'Book Three',
+            //     genre: 'History',
+            //     publish: 1967,
+            //     edition: 1987
+            //   },
+            //   { title: 'Book Six', genre: 'History', publish: 1907, edition: 1950 }
+            // ]   
+
+            // console.log(Books[4].genre);
+            //       Physics
+
+            //  const UserBooks = Books.filter( (fb)=> fb.edition > 1990)
+            //    console.log(UserBooks);
+
+              //  [
+              //   {
+              //     title: 'Book One',
+              //     genre: 'Friction',
+              //     publish: 1981,
+              //     edition: 2004
+              //   },
+              //   {
+              //     title: 'Book Two',
+              //     genre: 'Non-Friction',
+              //     publish: 1961,
+              //     edition: 1994
+              //   }
+              // ]
+ 
+              // const UserBooks = Books.filter( (fb)=> fb.edition > 1990 && fb.genre ==='Non-Friction')
+              //   console.log(UserBooks);
+
+              //   [
+              //     {
+              //       title: 'Book Two',
+              //       genre: 'Non-Friction',
+              //       publish: 1961,
+              //       edition: 1994
+              //     }
+              //   ]
+                
+                
